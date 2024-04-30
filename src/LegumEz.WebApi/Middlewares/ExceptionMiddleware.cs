20c11cc39df4b5371ae4909cc3d238f33486ebe6
david <david.gancarczyk@gmail.com>
2024-04-30 12:55:09 +0200

    Exception middleware handling EntityNotFoundExceptions

--- src/LegumEz.WebApi/Middlewares/ExceptionMiddleware.cs
+++ src/LegumEz.WebApi/Middlewares/ExceptionMiddleware.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Text.Json;
+using LegumEz.Infrastructure.Persistance.Exceptions;
 
 namespace LegumEz.WebApi.Middlewares;
 
@@ -20,32 +21,48 @@ internal class ExceptionMiddleware
         {
             await _next(httpContext);
         }
-        catch (Exception ex)
+        catch (Exception exception)
         {
-            _logger.LogError(ex, "Something went wrong: {ExceptionMessage}", ex.Message);
-            await HandleExceptionAsync(httpContext, ex);
+            await HandleExceptionAsync(httpContext, exception);
         }
     }
 
     private async Task HandleExceptionAsync(HttpContext context, Exception exception)
     {
-        context.Response.ContentType = "application/json";
-        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        
+        var errorDetails = new ErrorDetails();
 
-        await context.Response.WriteAsync(new ErrorDetails()
+        switch (exception)
         {
-            StatusCode = context.Response.StatusCode,
-            Message = "Internal Server Error from the custom middleware.",
-            Details = exception.StackTrace ?? string.Empty
-        }.ToString());
+            case EntityNotFoundException entityNotFoundException:
+                _logger.LogError(entityNotFoundException, "Could not found {EntityType} with ID : {EntityId}",
+                    entityNotFoundException.EntityType.Name, entityNotFoundException.EntityId);
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                errorDetails.StatusCode = context.Response.StatusCode;
+                errorDetails.Message = "Entity not found";
+                errorDetails.Details =
+                    $"Could not found {entityNotFoundException.EntityType.Name} with ID : {entityNotFoundException.EntityId}";
+                break;
+            default:
+                _logger.LogError(exception, "Something went wrong: {ExceptionMessage}", exception.Message);
+                errorDetails.StatusCode = context.Response.StatusCode;
+                errorDetails.Message = "Internal Server Error from the custom middleware.";
+                errorDetails.Details = exception.StackTrace ?? string.Empty;
+                break;
+        }
+
+        await context.Response.WriteAsync(errorDetails.ToString());
     }
 }
 
 internal record ErrorDetails
 {
-    public int StatusCode { get; init; }
-    public string Message { get; init; }
-    public string Details { get; init; }
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public string Details { get; set; }
+
     public override string ToString()
     {
         return JsonSerializer.Serialize(this);
