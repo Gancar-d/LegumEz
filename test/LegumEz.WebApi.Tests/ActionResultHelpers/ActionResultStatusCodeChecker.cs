ae9cb82125b8fa31d83fb6e5b5486c18b9deff8f
david <david.gancarczyk@gmail.com>
2024-04-30 12:53:05 +0200

    Fetch culture by Id with non existent id test

--- test/LegumEz.WebApi.Tests/ActionResultHelpers/ActionResultStatusCodeChecker.cs
+++ test/LegumEz.WebApi.Tests/ActionResultHelpers/ActionResultStatusCodeChecker.cs
@@ -1,20 +1,20 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LegumEz.WebApi.Tests.ActionResultHelpers
 {
     internal static class ActionResultStatusCodeChecker
     {
-        internal static void CheckIsOk200(this OkObjectResult result)
+        internal static void CheckIsOk200(this ObjectResult result)
         {
             result?.Should().NotBeNull();
             result?.StatusCode.Should().Be(200);
+        }
+        
+        internal static void CheckIsBadRequest400(this ObjectResult result)
+        {
+            result?.Should().NotBeNull();
+            result?.StatusCode.Should().Be(400);
         }
     }
 }
