53a0407bf9d78658c0b14c5036af4150db4c7e32
david <david.gancarczyk@gmail.com>
2024-06-24 11:08:58 +0200

    Added logs in repository

--- src/LegumEz.Infrastructure/Persistance/Repositories/CultureRepository.cs
+++ src/LegumEz.Infrastructure/Persistance/Repositories/CultureRepository.cs
@@ -5,6 +5,7 @@ using LegumEz.Domain.Plantation.spi;
 using LegumEz.Infrastructure.Persistance.Configuration;
 using LegumEz.Infrastructure.Persistance.Exceptions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace LegumEz.Infrastructure.Persistance.Repositories
 {
@@ -12,26 +13,35 @@ namespace LegumEz.Infrastructure.Persistance.Repositories
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly ILogger<CultureRepository> _logger;
 
         public CultureRepository(ApplicationDbContext dbContext,
-            IMapper mapper)
+            IMapper mapper,
+            ILogger<CultureRepository> logger)
         {
             _dbContext = dbContext;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public IEnumerable<Culture> FindAll()
         {
+            _logger.LogInformation("Fetching all cultures from database.");
+            
             var culturesFromDb = _dbContext.Cultures
                 .AsNoTracking()
                 .Select(x => x)
                 .ToList();
 
+            _logger.LogInformation("Fetched {CultureCount} cultures from database.", culturesFromDb.Count);
+            
             return _mapper.Map<IEnumerable<Culture>>(culturesFromDb);
         }
 
         public Culture FindById(Guid id)
         {
+            _logger.LogInformation("Fetching culture with Id {CultureId} from database", id);
+            
             var culture = _dbContext.Cultures
                 .AsNoTracking()
                 .SingleOrDefault(x => x.Id == id);
@@ -41,6 +51,8 @@ namespace LegumEz.Infrastructure.Persistance.Repositories
                 throw new EntityNotFoundException(id, typeof(DAL.Cultures.Culture),
                     $"Entity {nameof(Culture)} not found with ID {id}");
             }
+            
+            _logger.LogInformation("Succesfully fetched culture with Id {CultureId} from database", id);
             
             return _mapper.Map<Culture>(culture);
         }
