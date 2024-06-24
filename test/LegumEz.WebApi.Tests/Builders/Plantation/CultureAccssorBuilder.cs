014f5134568d07f9524ed9ec8323a0c67f236206
david <david.gancarczyk@gmail.com>
2024-06-24 15:14:39 +0200

    fix tests

--- test/LegumEz.WebApi.Tests/Builders/Plantation/CultureAccssorBuilder.cs
+++ test/LegumEz.WebApi.Tests/Builders/Plantation/CultureAccssorBuilder.cs
@@ -3,6 +3,7 @@ using LegumEz.Domain.Plantation.api;
 using LegumEz.Infrastructure.Persistance.Configuration;
 using LegumEz.Infrastructure.Persistance.Repositories;
 using LegumEz.WebApi.Tests.Builders.DbContext;
+using LegumEz.WebApi.Tests.Builders.Logger;
 using LegumEz.WebApi.Tests.Builders.Mapper;
 
 namespace LegumEz.WebApi.Tests.Builders.Plantation;
@@ -19,15 +20,17 @@ public class CultureAccessorBuilder
 
         return this;
     }
-    
+
     public IAccessCulture Build()
     {
         if (_dbContext is null)
         {
-            throw new CannotBuildException($"Cannot build {nameof(CultureRepository)}. Required parameter {nameof(_dbContext)} was null. Consider using {nameof(WithInMemoryDatabase)} method before building.");
+            throw new CannotBuildException(
+                $"Cannot build {nameof(CultureRepository)}. Required parameter {nameof(_dbContext)} was null. Consider using {nameof(WithInMemoryDatabase)} method before building.");
         }
-        
-        var repository = new CultureRepository(_dbContext, MapperBuilder.Build());
+
+        var logger = new LoggerBuilder<CultureRepository>().Build();
+        var repository = new CultureRepository(_dbContext, MapperBuilder.Build(), logger);
         return new CultureAccessor(repository);
     }
 }
